d9a646aafa7847b39ca041322aa7d3579d3c6a9c
Artur <pedrolopezv18@gmail.com>
2019-12-03 14:05:34 +0100

    Praca na galeziach (4)

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,10 +1,10 @@
 package edu.kis.vh.nursery;
 
-public class defaultCountingOutRhymer {
+public class DefaultCountingOutRhymer {
 
-    private int[] NUMBERS = new int[12];
+	public int total = -1;
 
-    public int total = -1;
+	private int[] NUMBERS = new int[12];
 
     public void countIn(int in) {
         if (!isFull())
@@ -19,7 +19,7 @@ public class defaultCountingOutRhymer {
         return total == 11;
     }
 
-    protected int peekaboo() {
+    protected int checkIn() {
         if (callCheck())
             return -1;
         return NUMBERS[total];
